962e113142450d07d1620061203ee6873bc8f258
Shahaf Antwarg <santwarg@gmail.com>
2021-10-25 10:20:26 +0300

    prepare

--- migrations/02_deploy.js
+++ migrations/02_deploy.js
@@ -2,17 +2,17 @@ const NFT = artifacts.require('NFT');
 const Royalties = artifacts.require('Royalties');
 
 const deployCollections = async (deployer, network, accounts) => {
-  const userAddress = '0xE7F506aAf42540B91bc6e5d2e109378d66de63EE';
-  const wethAddress = '0xc778417E063141139Fce010982780140Aa0cD5Ab';
+  /* const userAddress = '0x0x000000000000000000000000000000000000000';
+  const wethAddress = '0x000000000000000000000000000000000000000';
   const name = 'ROYALTIES TEST';
   const fees = 4;
   const totalSupply = 10000;
 
-  // await deployer.deploy(NFT, name, name);
+  await deployer.deploy(NFT, name, name);
 
-  const nft = await NFT.at('0xd5197aee9ea0dc7a5b40a3cbb6397e08ccbc499f');
+  const nft = await NFT.deployed();
 
-  // await nft.addMinter(userAddress);
+  await nft.addMinter(userAddress);
 
   await deployer.deploy(
     Royalties,
@@ -24,8 +24,8 @@ const deployCollections = async (deployer, network, accounts) => {
 
   const royalties = await Royalties.deployed();
 
-  // await nft.setRoyaltiesFees(fees);
-  await nft.setRoyaltiesAddress(royalties.address);
+  await nft.setRoyaltiesFees(fees);
+  await nft.setRoyaltiesAddress(royalties.address); */
 };
 
 module.exports = deployCollections;
